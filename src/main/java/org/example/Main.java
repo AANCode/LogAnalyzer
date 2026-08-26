8220af9f9568ff0e9b59ceaa58e9f4b18ed65049
AANCode <122744819+AANCode@users.noreply.github.com>
2026-08-26 14:05:14 +0200

    parse en txt fil

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        //C:\Users\iamdu\Documents\test.txt
         System.out.println("Skriv inn stien til din txt fil");
         String pathString = scanner.nextLine();
         Path path = Path.of(pathString);
@@ -20,10 +21,19 @@ public class Main {
             List<String> lines = Files.readAllLines(path);
             int countLines = lines.size();
             System.out.println("Loaded " + countLines + " log lines.");
+
+            for (String logs: lines){
+                String[] parts = logs.split(" ", 4);
+                System.out.println("Date: " + parts[0]);
+                System.out.println("Time: " + parts[1]);
+                System.out.println("Level: " + parts[2].replace("[","").replace("]",""));
+                System.out.println("Message: " + parts[3]);
+            }
         } catch (IOException e){
             System.out.println("tekst filen finnes ikke");
 
         }
+
 
     }
 }
